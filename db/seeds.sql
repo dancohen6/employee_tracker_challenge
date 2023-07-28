f9c9576626a5e08af9f3c0c64ce4b09f4cf05d9c
dancohen6 <dzcsix@gmail.com>
2023-07-28 13:33:39 -0400

    final schema/seed updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 INSERT INTO department (name) VALUES ('Management'), ('Coaching Staff'), ('Player');
 
 INSERT INTO role (title, salary, department_id) VALUES
-    ('Center', 8000000, 3),
+    ('General Manager', 3000000, 1),
     ('Coach', 1000000, 2),
-    ('General Manager', 3000000, 1);
+    ('Center', 8000000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
     ('Tom', 'Fitzgerald', 1, NULL),
     ('Lindy', 'Ruff', 2, 1),
-    ('Nico', 'Hischier', 3, null),
-    ('Jack', 'Hughes', 3, null);
+    ('Nico', 'Hischier', 3, 2),
+    ('Jack', 'Hughes', 3, 2);
